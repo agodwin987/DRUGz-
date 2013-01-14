b457dc01088e1c543b9dd17963a2f8a0ff53bccb
Alex Godwin <agodwin987@gmail.com>
2013-01-14 17:02:06 -0500

    Commit test

--- DRUGz Plugins/src/drugz/git/test/GitTest.java
+++ DRUGz Plugins/src/drugz/git/test/GitTest.java
@@ -2,6 +2,6 @@ package drugz.git.test;
 
 public class GitTest {
 	  public static void main(String[] args) {
-	    System.out.println("Git is fun");
+	    System.out.println("Git is cool");
 	  }
 	}
